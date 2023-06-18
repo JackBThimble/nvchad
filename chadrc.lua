fbe090863c0bfed00b7ee575568702f09e17b0cc
Jake Crowell <jasperdaniels91@icloud.com>
2023-06-18 13:45:30 -0400

    Initial commit

---
+++ chadrc.lua
@@ -0,0 +1,34 @@
+---@type ChadrcConfig
+local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "radium",
+  theme_toggle = { "radium", "one_light" },
+  transparency = true,
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+  lsp = {
+    signature = {
+      disabled = false,
+      silent = true,
+    }
+  },
+  statusline = {
+    theme = "vscode_colored"
+  },
+  cmp = {
+    icons = true,
+    style = "flat_dark",
+    lspkind_text = true,
+  }
+}
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
+return M
