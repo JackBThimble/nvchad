da32d71bcd36c6a580fad5f2ca855c87a286132e
Jake Crowell <jasperdaniels91@icloud.com>
2023-06-18 14:43:45 -0400

    updates

--- chadrc.lua
+++ chadrc.lua
@@ -10,6 +10,12 @@ M.ui = {
   transparency = true,
   hl_override = highlights.override,
   hl_add = highlights.add,
+  lsp_semantic_tokens = true,
+
+  nvdash = {
+    load_on_startup = true,
+  },
+
   lsp = {
     signature = {
       disabled = false,
