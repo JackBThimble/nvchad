14ae2b824848d1193c9daa45de8a313362fe3408
Jake Crowell <jasperdaniels91@icloud.com>
2023-06-21 21:38:31 -0400

    colorsheme change

--- chadrc.lua
+++ chadrc.lua
@@ -5,7 +5,7 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "radium",
+  theme = "rosepine",
   theme_toggle = { "radium", "one_light" },
   transparency = true,
   hl_override = highlights.override,
